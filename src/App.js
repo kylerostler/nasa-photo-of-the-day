0d3211187cc7feb8367165686978bf982501fdc3
kylerostler <93017344+kylerostler@users.noreply.github.com>
2021-12-08 16:29:28 -0700

    finished side effect for app making components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import Axios from "axios";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 import { BASE_URL } from './constants';
@@ -10,12 +11,30 @@ function App() {
   const [photo, setPhoto] = useState([]);
   const [currentPicId, setCurrentPicId] = useState(null);
 
+  useEffect(() => {
+    const fetchPhoto = () => {Axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+      .then(response => {
+        setPhoto(response.data)
+      })
+      .catch(error => {
+        console.error(error)
+      })
+    }
+    fetchPhoto();
+  }, []);
+
+console.log(photo)
+
+
+
+
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
+      {/* <NasaPhoto /> */}
     </div>
   );
 }
